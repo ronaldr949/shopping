f3783f0ba17a689aee3d59306614b6ad2864531f
Roney <ronaldr949@gmail.com>
2019-10-14 08:49:38 -0400

    updated

---
+++ src/test/java/com/bias/app/AppTest.java
@@ -0,0 +1,20 @@
+package com.bias.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
